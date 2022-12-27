a710eea5113de6ae85eaa81d0e4b7e99f26afaf1
IfySamuelDania <anahimisamuel@yahoo.com>
2022-12-27 09:28:30 -0800

    all tasks

---
+++ 0x00-python-hello_world/6-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+str1 = "Holberton"
+str2 = "School"
+str1 += " " + str2
+print("Welcome to {}!".format(str1))
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
